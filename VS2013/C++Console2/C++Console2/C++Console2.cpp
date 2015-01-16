ea23a9827a2bc65bd9df178f36f15eba7880131c
Dan-Joe <Dan-Joe.Lopez@sap.com>
2015-01-16 09:24:17 -0800

    added UI5 source files

---
+++ VS2013/C++Console2/C++Console2/C++Console2.cpp
@@ -0,0 +1,43 @@
+// C++Console2.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+struct movies_t {
+	string title;
+	int year;
+} films[3];
+
+void printmovie(movies_t movie);
+
+int main()
+{
+	string mystr;
+	int n;
+
+	for (n = 0; n<3; n++)
+	{
+		cout << "Enter title: ";
+		getline(cin, films[n].title);
+		cout << "Enter year: ";
+		getline(cin, mystr);
+		stringstream(mystr) >> films[n].year;
+	}
+
+	cout << "\nYou have entered these movies:\n";
+	for (n = 0; n<3; n++)
+		printmovie(films[n]);
+
+	cout << "Press enter to exit";
+	getline(cin, mystr);
+	return 0;
+}
+
+void printmovie(movies_t movie)
+{
+	cout << movie.title;
+	cout << " (" << movie.year << ")\n";
+}
