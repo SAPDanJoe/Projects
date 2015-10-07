c12e53337e734f0a23642855030ca4c411acf0eb
Dan-JoeDan-Joe <Dan-Joe.Lopez@sap.com>
2015-10-07 11:56:03 -0700

    numerous changes in projects dir; will move these to their own repos

---
+++ ProcessRestarter/ProcessRestarter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProcessRestarter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
+
+            string processName = ProcessRestarter.Properties.Settings.Default.processName;
+            string processEXE = ProcessRestarter.Properties.Settings.Default.processEXE;
+            Process[] namedProcesses = Process.GetProcessesByName(processName);
+            
+            if (namedProcesses.Count() == 0)
+            {
+                Program.EventLog.WriteEntry(
+                    "The process named {" + processName + 
+                    "} was not found to be running at {" + DateTime.Now + 
+                    "}.  Attempting to restart the process from {" + processEXE + "}");
+                Process.Start(processEXE);
+            }
+
+        }
+    }
+}
---
+++ ProcessRestarter/ProcessRestarter/Service1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessRestarter
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string configFilePath = Environment.GetEnvironmentVariable("APPDATA") + @"\SAPIT\ProcessRestarter\config.txt";
+
+            if ( ! System.IO.File.Exists( configFilePath ))
+            {
+                this.EventLog.WriteEntry(
+                    "The service {" 
+                    + this.ServiceName + 
+                    "}failed to start: No configuration file exists in {"
+                    + configFilePath + 
+                    "}.  Please check the file and permissions and try again.");
+                this.Stop();
+            }
+            string config = System.IO.File.ReadLines(configFilePath).ToString();
+            ProcessRestarter.Properties.Settings.Default.processName = config.Split(':')[0].ToString();
+            ProcessRestarter.Properties.Settings.Default.processEXE = config.Split(':')[1].ToString();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
